defdba34ad45adff531a2a3c89398b04ab4092ba
SrijanChakraborty <99581334+SrijanChakraborty@users.noreply.github.com>
2024-09-11 23:26:31 +0530

    commit

--- webside/app.py
+++ webside/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 import pickle
 import requests
+from waitress import serve
 
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return '<h1>Hello!</h1>'
 
 
 if __name__ == '__main__':
+    # production
+    # serve(app, host="localhost", port=4000)
+    # Development
     app.run(debug=True, host='localhost', port=4000)
